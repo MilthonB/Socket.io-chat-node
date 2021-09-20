dd2c767147f341382f4b074dc6f953ce399ad7ad
MB <milthonbor@gmail.com>
2021-09-20 02:18:50 -0700

    public folder

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,61 +1,58 @@
-let socket = io();
+var socket = io();
 
-
-let params = new URLSearchParams(window.location.search);
+var params = new URLSearchParams(window.location.search);
 
 if (!params.has('nombre') || !params.has('sala')) {
     window.location = 'index.html';
-    throw new Error('El nombre es necesario');
+    throw new Error('El nombre y sala son necesarios');
 }
 
+var usuario = {
+    nombre: params.get('nombre'),
+    sala: params.get('sala')
+};
 
-let usuario = {
-    nombre : params.get('nombre'),
-    sala : params.get('sala')
-}
 
 
-socket.on('connect', function () {
-    console.log('Conectado al servidor', usuario);
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
 
-    socket.emit('entrar-chat', usuario, ( resp  ) => {
-        console.log('usuario conectados',resp);
+    socket.emit('entrarChat', usuario, function(resp) {
+        console.log('Usuarios conectados', resp);
     });
 
 });
 
 // escuchar
-socket.on('disconnect', function () {
+socket.on('disconnect', function() {
 
     console.log('Perdimos conexión con el servidor');
 
 });
 
 
-
-
 // Enviar información
-// socket.emit('crear-mensaje', {
-//     usuario: 'Fernando',
+// socket.emit('crearMensaje', {
+//     nombre: 'Fernando',
 //     mensaje: 'Hola Mundo'
-// }, function (resp) {
+// }, function(resp) {
 //     console.log('respuesta server: ', resp);
 // });
 
 // Escuchar información
-socket.on('crear-mensaje', function (mensaje) {
-
+socket.on('crearMensaje', function(mensaje) {
     console.log('Servidor:', mensaje);
-
 });
 
-//Escuchar cambios de los us    uarios
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
+socket.on('listaPersona', function(personas) {
+    console.log(personas);
+});
 
+// Mensajes privados
+socket.on('mensajePrivado', function(mensaje) {
 
-socket.on('lista-personas', (personas) => {
-    console.log(personas );
-})
+    console.log('Mensaje Privado:', mensaje);
 
-socket.on('mensaje-privado',(payload) =>  {
-    console.log('mensaje info: ',payload);
 });
